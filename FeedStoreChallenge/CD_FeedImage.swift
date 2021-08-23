0581fa3a35991323eada36893102b4dd940bc307
cgrande <cgrande@codacove.com>
2021-08-23 18:27:03 +1000

    add mapping functionality between CD and Local models

--- FeedStoreChallenge/CD_FeedImage.swift
+++ FeedStoreChallenge/CD_FeedImage.swift
@@ -4,26 +4,33 @@
 import Foundation
 import CoreData
 
-
 @objc(CD_FeedImage)
-public class CD_FeedImage: NSManagedObject {
-
-}
+public class CD_FeedImage: NSManagedObject {}
 
 extension CD_FeedImage {
 
-    @nonobjc public class func fetchRequest() -> NSFetchRequest<CD_FeedImage> {
-        return NSFetchRequest<CD_FeedImage>(entityName: "CD_FeedImage")
-    }
-
-    @NSManaged public var id: UUID?
-    @NSManaged public var imageDescription: String?
-    @NSManaged public var location: String?
-    @NSManaged public var url: URL?
-    @NSManaged public var cache: CD_Cache?
-
+	@NSManaged public var id: UUID?
+	@NSManaged public var imageDescription: String?
+	@NSManaged public var location: String?
+	@NSManaged public var url: URL?
+	@NSManaged public var cache: CD_Cache?
 }
 
-extension CD_FeedImage : Identifiable {
+extension CD_FeedImage: Identifiable {}
 
+extension CD_FeedImage {
+	internal static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+		return NSOrderedSet(array: localFeed.map { local in
+			let feedImage = CD_FeedImage(context: context)
+			feedImage.id = local.id
+			feedImage.imageDescription = local.description
+			feedImage.location = local.location
+			feedImage.url = local.url
+			return feedImage
+		})
+	}
+	
+	internal var local: LocalFeedImage {
+		return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
+	}
 }
