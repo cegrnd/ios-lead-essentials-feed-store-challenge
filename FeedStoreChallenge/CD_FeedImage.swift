055517df4b4cc55640c675e26a1ef200553799fc
cgrande <cgrande@codacove.com>
2021-08-23 18:41:49 +1000

    mark id and url as non-optional in the class definition file (somehow exported as optional even if set as non-optional in the coredata model file)

--- FeedStoreChallenge/CD_FeedImage.swift
+++ FeedStoreChallenge/CD_FeedImage.swift
@@ -8,11 +8,10 @@ import CoreData
 public class CD_FeedImage: NSManagedObject {}
 
 extension CD_FeedImage {
-
-	@NSManaged public var id: UUID?
+	@NSManaged public var id: UUID
 	@NSManaged public var imageDescription: String?
 	@NSManaged public var location: String?
-	@NSManaged public var url: URL?
+	@NSManaged public var url: URL
 	@NSManaged public var cache: CD_Cache?
 }
 
@@ -29,7 +28,7 @@ extension CD_FeedImage {
 			return feedImage
 		})
 	}
-	
+
 	internal var local: LocalFeedImage {
 		return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
 	}
